ba849387c5eef77a4f53596c50f143c843e5d097
Yameen <yameen.irteza@gmail.com>
2019-07-19 17:10:17 +0600

    add

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Flight Management System</title>
+    
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Lato:300,400,700,900" rel="stylesheet">
+    <script src="../ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Font Aweosme -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+	
+	<!-- Custom Stylesheet -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	
+	<!-- Color Switcher -->
+	<link rel="stylesheet" href="css/colors/red.css" id="color-switch">
+	
+	
+	<link rel="stylesheet" href="form.css" >
+        <script src="form.js"></script>
+
+</head>
+<body>
+	<!-- Preloader -->
+    <!--<div class="preloader">
+    	<div id="loader">
+			<div id="box"></div>
+			<div id="hill"></div>
+		</div>
+    </div>-->
+    <!-- Header -->
+    <header id="home">
+        <nav class="navbar navbar-default" id="main-nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-morki">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="index-2.html"><img src="images/001.gif" class="img-responsive img-circle margin"  style="height: 75px;margin-top:-10px;"></a>
+                             <h1 style="color: white;display:inline;font-family: Comic Sans MS;padding-top:20px;font-size:50px;">inFlynite</h1>
+						</div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse navbar-morki">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="active"><a href="#home" data-scroll>Home</a></li>
+                                <li><a href="about.php" >About</a></li>
+								<li><a href="#features" data-scroll>Features</a></li>
+                                <li><a href="#faqs" data-scroll>Faq</a></li>
+                                <li><a href="#contact" data-scroll>Contact</a></li>
+                                <li class="nav-download-btn"><a href="login.php" class="login">Log In!</a></li>		
+							</ul>
+                        </div><!-- .navbar-collapse -->
+					</div>
+				</div>
+			</div>
+		</nav>
+    </header><!-- Ends: header -->
+    
+    <!-- Intro Section -->
+    <section class="intro intro-style2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-7 intro-info wow fadeIn">
+                    <div class="intro-info-content">
+                        <h1>when its about flights <br/> <span>inFlynite</span> is your easiest, quickest & cost effective solution!</h1>
+                        <h4><b>inFlynite is an application-based management service that promises speed, security, and most of all its efficiency!</b></h4>
+						<div class="container">
+    
+						</div>
+                    </div>
+                </div><!-- Ends: .intro-info -->
+            </div>
+        </div>
+    </section><!-- Ends: .intro -->
+    
+
+</body>
+
+</html>

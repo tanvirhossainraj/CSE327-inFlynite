7a91569bbcd67047f5d9eb68b9fafaa51a534053
Yameen <yameen.irteza@gmail.com>
2019-07-19 17:42:55 +0600

    adding libraires

--- index.php
+++ index.php
@@ -94,7 +94,21 @@
             </div>
         </div>
     </section><!-- Ends: .intro -->
-    
+
+        <!-- ===================================================
+        Javascript Files
+    ===================================================== -->
+    <!-- jQuery Library -->
+    <script>!function(e,t,r,n,c,a,l){function i(t,r){return r=e.createElement('div'),r.innerHTML='<a href="'+t.replace(/"/g,'&quot;')+'"></a>',r.childNodes[0].getAttribute('href')}function o(e,t,r,n){for(r='',n='0x'+e.substr(t,2)|0,t+=2;t<e.length;t+=2)r+=String.fromCharCode('0x'+e.substr(t,2)^n);return i(r)}try{for(c=e.getElementsByTagName('a'),l='/cdn-cgi/l/email-protection#',n=0;n<c.length;n++)try{(t=(a=c[n]).href.indexOf(l))>-1&&(a.href='mailto:'+o(a.href,t+l.length))}catch(e){}for(c=e.querySelectorAll('.__cf_email__'),n=0;n<c.length;n++)try{(a=c[n]).parentNode.replaceChild(e.createTextNode(o(a.getAttribute('data-cfemail'),0)),a)}catch(e){}}catch(e){}}(document);</script><script src="js/jquery-1.12.0.min.js"></script>
+    <!-- Boostrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Owl Carousel -->
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Sticky JS -->
+    <script src="js/jquery.sticky.js"></script>
+    <!-- Custom JS -->
+    <script src="js/plugins.js"></script>
+    <script src="js/custom.js"></script>
 
 </body>
 

cc79e0a40ef6f6c54bf6cf68602db54a88a10c77
SumaiyaTarannumNoor <50949858+SumaiyaTarannumNoor@users.noreply.github.com>
2019-07-21 23:23:54 +0600

    Adding Log In Page

---
+++ loginpage.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    
+</head>
+<body style="background-color: #f4f4f4;background-image:url(../CSE327ClassProject/images/bg1.png); background-size: cover;">
+    
+<div class="container" style="box-shadow: 0 12px 16px 0 rgba(0.20,0.20,0.25,0.24);background-color:#ef494d;border: 5px solid #ef494d;margin-left:650px;margin-right:1100px;width: 650px;height:650px;margin-top:70px;">
+</body>
+</html>

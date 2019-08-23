79262ee072182ccc422431812e9b2fc96f74ddf2
yameen1809 <53079246+yameen1809@users.noreply.github.com>
2019-08-23 21:35:09 +0600

    For connecting the flight info to the table

---
+++ showtable2.php
@@ -0,0 +1,32 @@
+	<?php
+	              
+					$con=mysqli_connect("localhost","root","","flightmanagementsystem");
+					// Check connection
+					if (mysqli_connect_errno())
+					{
+					echo "Failed to connect to MySQL: " . mysqli_connect_error();
+					}
+
+					$result = mysqli_query($con,"SELECT * FROM flights");
+
+					/*echo "<table border='1'>
+					<tr>
+					<th>Firstname</th>
+					<th>Lastname</th>
+					</tr>";*/
+					while($row = mysqli_fetch_array($result))
+					{
+				    
+					echo "<tr>";
+					echo "<td>". $row['Flight_Time'] . "</td>";
+					echo "<td>" . $row['Flight_No'] . "</td>";
+					echo "<td>" . $row['Destination'] . "</td>";
+					echo "<td>" . $row['Gate'] . "</td>";
+					echo "<td>" . $row['Flight_Status'] . "</td>";
+				    echo "</tr>";
+					}
+					
+					echo "</table>";
+
+					mysqli_close($con);
+					?>
